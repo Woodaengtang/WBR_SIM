6b4a561edeed0063ef8298ed7be242fabc5fd0bb
Woodaengtang <jhwoo200@gmail.com>
2025-01-15 23:44:51 +0900

    chore: lines adjusted simply

--- src/wbr_gazebo/include/wbr_state_talker.hpp
+++ src/wbr_gazebo/include/wbr_state_talker.hpp
@@ -1,16 +1,23 @@
+#ifndef WbrStateTalker_HPP
+#define WbrStateTalker_HPP
+
+#include <iostream>
+#include <string>
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
+// #include "wbr_controller/msg/WbrState.hpp"
 
 using std::placeholders::_1;
 
-class WbrStateTalker : public rclcpp::Node
-{
-  public:
-    WbrStateTalker();
+class WbrStateTalker : public rclcpp::Node {
+    public:
+      WbrStateTalker();
+    private:
+      void topic_callback(const sensor_msgs::msg::JointState::SharedPtr msg);
+      rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscription_;
 
-  private:
-    void topic_callback(const sensor_msgs::msg::JointState::SharedPtr msg);
-     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscription_;
 };
+
+#endif
